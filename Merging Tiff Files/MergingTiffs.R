23b8817b91c7e6c81ea3f1532ea45d0f05f5d919
Tyler <tyler.w.bradshaw@duke.edu>
2019-02-12 14:48:07 -0500

    Merging tifs for Aki.

--- Merging Tiff Files/MergingTiffs.R
+++ Merging Tiff Files/MergingTiffs.R
@@ -1,31 +1,46 @@
-#####################################################################################
-## Prepare the workspace.
-#####################################################################################
+#----------------------------------------------------------------------------------
+## Merging .tif files for Aki. 021219
 
+# Prepare the workspace.
 rm(list = ls())
 dev.off()
 cat("\014") # alternative is cat("\f")
 options(stringsAsFactors = FALSE)
 
-path <- "D:/Documents/R/StackOverflow/Merging Tiff Files"
+# Set path.
+path <- "D:/Documents/R/StackOverflow/StackOverflow/Merging Tiff Files"
 setwd(path)
 
-library(raster)
-files <- list.files()
+# Install package "tiff"
+#install.packages("tiff")
 
-str_name <- files[1]
-imported_raster <- raster(str_name)
-
+# Load the library.
 library(tiff)
-what <- files[c(1:2)]
-where <- paste(path,"merged.tif",sep="/")
 
-image1 <- readTIFF(source=files[1], native = FALSE, all = FALSE, convert = FALSE,
-         info = FALSE, indexed = FALSE, as.is = FALSE)
-image2 <- readTIFF(source=files[2], native = FALSE, all = FALSE, convert = FALSE,
-                   info = FALSE, indexed = FALSE, as.is = FALSE)
+# Load the data.
+list.files()
+files <- list.files(pattern=".tif")
 
-what <- list(image1,image2)
-writeTIFF(what, where, bits.per.sample = 8L,
+# Create an empty list for storing the files. 
+list_images <- list()
+
+# Loop to read files. 
+for (i in 1:length(files)){
+  list_images[[i]] <- readTIFF(source=files[i])
+}
+
+# Create a new directory for saving the output.
+output_dir <- paste(path,"Merged Files",sep="/")
+dir.create(file.path(output_dir))
+
+# Create a name for the new file.
+name <- paste0("merged",
+               strsplit(files[1],"x63")[[1]][2]) # Use strsplit to split the string at "x63".
+
+# Set the file path for the merged image. 
+out_file <- paste(output_dir,name,sep="/")
+
+# Write to .tif
+writeTIFF(list_images, out_file, bits.per.sample = 8L,
           compression = "none",
           reduce = FALSE)
